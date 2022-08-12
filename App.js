0996c2bae61985e7135a049e5781a0151a38c661
Chathuranga97lj <chathuit97@gmail.com>
2022-08-13 02:11:21 +0530

    add splash-screen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import {Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View>
       <Text>Hello</Text>
