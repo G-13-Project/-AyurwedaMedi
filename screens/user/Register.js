18c5cc4ddc8250b5f970a44c5a8b44a996129253
Chathuranga97lj <chathuit97@gmail.com>
2022-08-20 13:47:38 +0530

    add register page form

--- screens/user/Register.js
+++ screens/user/Register.js
@@ -6,16 +6,28 @@ import {
   Image,
   SafeAreaView,
   TouchableOpacity,
-} from 'react-native';
+  ScrollView,
+  StyleSheet,
+  TextInput, Button,
+} from "react-native";
+import SelectList from 'react-native-dropdown-select-list/index';
 import {global} from '../../styles/global';
 
 const Register = ({navigation}) => {
+  // for dropdown menu selection
+  const [Selected, setSelected] = React.useState('');
+  const data = [
+    {key: '1', value: 'Doctor'},
+    {key: '2', value: 'Seller'},
+    {key: '3', value: 'Researcher'},
+  ];
   return (
     <SafeAreaView style={global.bgImage}>
       <ImageBackground
         source={require('../../assets/background.png')}
         resizeMode="cover"
         style={global.bgImage}>
+        {/*Header components*/}
         <View style={global.header}>
           <TouchableOpacity onPress={() => navigation.openDrawer()}>
             <Image
@@ -33,9 +45,143 @@ const Register = ({navigation}) => {
           <Text style={global.subHeadText}>Registration</Text>
         </View>
 
+        {/*form content components*/}
+        <ScrollView>
+          <View style={styles.dropDown}>
+            {/*dropdown content*/}
+            <SelectList
+              data={data}
+              setSelected={setSelected}
+              placeholder="Register As"
+              boxStyles={{
+                backgroundColor: '#ffffff',
+                borderColor: '#1CAB4C',
+                borderWidth: 2,
+                borderRadius: 25,
+                height: 50,
+              }}
+              inputStyles={{fontSize: 16, color: '#47D50D'}}
+              dropdownTextStyles={{fontSize: 16, color: '#47D50D'}}
+              dropdownStyles={{
+                backgroundColor: '#ffffff',
+                borderWidth: 0,
+                borderRadius: 0,
+              }}
+            />
+          </View>
+          {/*input fields for doctor registration*/}
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>Name :</Text>
+            <TextInput
+              placeholder="Moda Pawani"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>NIC : </Text>
+            <TextInput
+              placeholder="981234567V"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>District : </Text>
+            <TextInput
+              placeholder="Dakunu Rata"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>Contact No : </Text>
+            <TextInput
+              placeholder="0711234567"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>Email : </Text>
+            <TextInput
+              placeholder="modapawi98@gmail.com"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>Password : </Text>
+            <TextInput
+              placeholder="**********"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.form}>
+            <Text style={styles.textLabel}>Confirm Password : </Text>
+            <TextInput
+              placeholder="**********"
+              style={styles.textInput}
+              placeholderTextColor="#4db871"></TextInput>
+          </View>
+          <View style={styles.line}></View>
+
+          <View style={styles.regBtn}>
+            <Button title="Register" color="#47D50D" />
+          </View>
+
+        </ScrollView>
       </ImageBackground>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  dropDown: {
+    marginTop: '15%',
+    marginLeft: '15%',
+    marginRight: '15%',
+  },
+  form: {
+    flex: 3,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: '10%',
+    marginTop: '2%',
+    height: 50,
+  },
+  textLabel: {
+    color: 'black',
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    flex: 2,
+    fontSize: 16,
+  },
+  line: {
+    backgroundColor: '#000000',
+    height: 2,
+    width: '80%',
+    marginLeft: '10%',
+  },
+  regBtn: {
+    borderColor: '#47D50D',
+    width: 150,
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginTop: 20,
+  },
+});
 
 export default Register;
