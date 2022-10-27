3f4b65d23f5d380450c6bfe9dcf2a68d96d6e27f
Chathuranga97lj <chathuit97@gmail.com>
2022-10-27 09:26:14 +0530

    set up image slider

--- screens/user/Home.js
+++ screens/user/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   View,
   Text,
@@ -6,10 +6,21 @@ import {
   SafeAreaView,
   Image,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 import {global} from '../../styles/global';
+import {SliderBox} from 'react-native-image-slider-box';
 
 const Home = ({navigation}) => {
+  const [image, setImage] = React.useState([
+    require('../../assets/slider/1.png'),
+    require('../../assets/slider/2.png'),
+    require('../../assets/slider/3.png'),
+    require('../../assets/slider/4.png'),
+    require('../../assets/slider/5.png'),
+    require('../../assets/slider/6.png'),
+  ]);
+
   return (
     <SafeAreaView style={global.bgImage}>
       <ImageBackground
@@ -33,9 +44,44 @@ const Home = ({navigation}) => {
             />
           </TouchableOpacity>
         </View>
+        <View style={styles.slider}>
+          <SliderBox
+            images={image}
+            sliderBoxHeight={540}
+            parentWidth={290}
+            onCurrentImagePressed={index =>
+              console.warn(`image ${index} pressed`)
+            }
+            autoplay
+            circleLoop
+            dotColor="#47D50D"
+            inactiveDotColor="#90A4AE"
+            resizeMethod={'resize'}
+            resizeMode={'cover'}
+            paginationBoxStyle={{
+              position: 'absolute',
+              bottom: 0,
+              padding: 0,
+              paddingLeft: -30,
+              paddingVertical: 5
+            }}
+            ImageComponentStyle={{
+              borderRadius: 15,
+              width: '100%',
+              marginTop: 5,
+            }}
+          />
+        </View>
       </ImageBackground>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  slider: {
+    paddingTop: 60,
+    paddingLeft: 55,
+  }
+})
 
 export default Home;
